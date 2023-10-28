2f265f8dd057bd02e7f90c691f8368018546845d
maximiliano <maxicardenas92@gmail.com>
2023-10-27 21:36:50 -0300

    clase afterclass

---
+++ after-class/js/index.js
@@ -0,0 +1,59 @@
+// let estaRegistrado = true;
+// let esMayorDeEdad = 16;
+
+// if(estaRegistrado == true && esMayorDeEdad >= 18){
+//     console.log("ESTAS DENTRO DEL SITIO")
+// }else{
+//     console.log("No entras al sitio")
+// };
+
+// let estaRegistrado = prompt("¿Esta registrado? (responda con si  no)");
+// let esMayorDeEdad = prompt ("¿Eres mayor de edad?(responde si o no) " );
+
+// if(estaRegistrado === "si" && esMayorDeEdad === "si"){
+//     console.log("estas dentro")
+// }else{
+//     console.log("estas fuera");
+// }
+
+// programa donde se solicite a un estudiante que ingrese su calificacion. 
+// esta califaccion va ir de 0 a 100 y tiene que mostrar un mensaje por cada tipo de nota.
+
+//Si la califacacion es mayor 0 igual a  90, "muy bien 10 crack"
+//si la calificacion es entre 80 y 89 (muy bien pero no sos tan crack)
+//si calificacion es entre 70y 79 "bien aprobaste"
+// si la calificacion es 60 y 69  casi campeon volve a entregar
+//por debajo de 60 (anda pa sha a diciembre);
+
+// let 
+// const COLIFICACION = parseInt(prompt("ingrese su nota"));
+
+// if(CALIFICACION >= 90){
+//     console.log(CALIFICACION+" "+"muy bien 10, crack");
+// }else if (CALIFICACION >= 80){
+//     console.log(CALIFICACION+" "+"muy bien pero no sos tan crack")
+// }else if (CALIFICACION >= 70){
+//     console.log(CALIFICACION+" "+"bien aprobaste")
+// }else if (CALIFICACION>=60){
+//     console.log(CALIFICACION+" "+"casi campeon pero volve a entregar")
+// }else{
+//     console.log (CALIFICACION+" "+"anda pasha a diciembre")
+// }
+
+//crear un programa, donde nos diga el horario del dia (mañana, tarde o noche)
+//Esto lo va a determinar el usuario. 
+
+// si la hora esta entre las 5:00 y las 11:59 devolver "buen dia"
+// de 12 a 19:59 devolver "buenas tardes"
+//de 20 a 4:58 devolver "buenas noches"
+
+
+const HORA = parseInt(prompt("¿que hora es? enformato de 24hs"));
+
+if (HORA >= 5 && HORA < 12 ){
+    console.log("buen dia");
+} else if (HORA >= 12 && HORA < 20){
+    console.log("buenas tardes");
+}else{
+    console.log("buenas noches");
+}
